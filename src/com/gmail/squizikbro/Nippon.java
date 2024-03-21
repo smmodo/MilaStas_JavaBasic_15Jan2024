dafe35ec2718c8874a0f8a6c9f489bbe18a886f4
[mi_felice] <7615757@gmail.com>
2024-03-21 19:20:17 +0200

    Shuttles 21Mar - 100

--- src/com/gmail/squizikbro/Nippon.java
+++ src/com/gmail/squizikbro/Nippon.java
@@ -3,11 +3,6 @@ package com.gmail.squizikbro;
 public class Nippon {
     public static void main(String[] args) {
 
-        //У Японії числа 4 та 9 вважаються нещасливими.
-        // Вам потрібно пронумерувати 100 космічних шатлів для перевезення людей на Марс так,
-        // щоб у номерах шатлів не траплялося нещасливих чисел. Напишіть функцію,
-        // яка виводить усі номери таких шатлів.
-
         int shuttle = 1;
         while (shuttle < 100) {
             shuttle++;
@@ -16,22 +11,6 @@ public class Nippon {
                 System.out.println(shuttle);
             }
 
-
-
         }
     }
 }
-//        int shuttle = 0;
-//        int inprogress = 0;
-//
-//        while (shuttle < 999999) {
-//            shuttle++;
-//            for (int i = 1; i < shuttles.length; i++);
-//                inprogress = shuttle;
-//                if (inprogress % 10 != 4 && inprogress % 10 != 9) {
-//                    System.out.println(shuttle);
-//                }
-//            inprogress=inprogress/10;
-//        }
-//    }
-//}
